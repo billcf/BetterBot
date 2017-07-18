f312bdbae411f40eb641167e1c701b55e04f10b8
williamfields <bill@williamfields.com>
2017-07-18 15:25:31 -0400

    Added initial files.

---
+++ AnalysisThread.java
@@ -0,0 +1,31 @@
+
+public class AnalysisThread extends Thread 
+{
+	int index;
+	int mode;
+	
+	public AnalysisThread(int modeSelect, int indexSelect) throws Exception
+	{
+		index = indexSelect;
+		mode = modeSelect;
+	}					
+
+	public void run()
+	{
+		if (mode == 0) //RMS Calculation
+		{				
+			if (index == 0)
+			{
+				BetterBot.s.RMSL = BetterBot.s.RMS(0);
+			}
+			else
+			{
+				BetterBot.s.RMSR = BetterBot.s.RMS(1);
+			}
+		}
+		else //EQ Analysis
+		{
+			BetterBot.s.analyzeBand(index);
+		}
+	}
+}
